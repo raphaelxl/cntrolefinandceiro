f814c4024c2c6f2749375d7d9681a6b80d15123c
raphaelxl <nivelop844@gmail.com>
2026-02-19 17:24:00 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: "/cntrolefinandceiro/",
+    base: "/",
     plugins: [
       react(),
     ],
