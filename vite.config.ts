3a7838f7bcdcc4eabf6c09de303da057d7d41698
raphaelxl <nivelop844@gmail.com>
2026-02-19 17:20:32 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -10,11 +9,10 @@ export default defineConfig(({ mode }) => {
     base: "/cntrolefinandceiro/",
     plugins: [
       react(),
-      mode === "development" && componentTagger(),
-    ].filter(Boolean),
+    ],
     build: {
       outDir: 'dist',
-      assetsDir: 'assets_vfinal_fix',
+      assetsDir: 'assets_vfinal_final',
     },
     resolve: {
       alias: {
