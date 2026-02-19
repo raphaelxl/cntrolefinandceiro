61ec25a3add52b2c9c4e5f6b2f14c63a44ed0e45
raphaelxl <nivelop844@gmail.com>
2026-02-19 17:15:20 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
+
   return {
     base: "/cntrolefinandceiro/",
     plugins: [
