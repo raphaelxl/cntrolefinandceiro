ee5713742ccfecec29b4641ff27d554c5bf698e5
raphaelxl <nivelop844@gmail.com>
2026-02-20 14:27:11 -0400

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-
 const SUPABASE_URL = "https://bvvlswtnwhpbjjhgsoo.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_maFvQfL7n_ctmGAhm5VmcA_HSkIqI2w";
-
 export const supabase = createClient(
   SUPABASE_URL,
   SUPABASE_PUBLISHABLE_KEY
