f67011830c6380731494792a452a61ebb8621e92
raphaelxl <nivelop844@gmail.com>
2026-02-23 15:37:30 -0400

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -6,5 +6,4 @@ const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_5iqQHnRSl48pHoK1F466IA_4c12Dg47
 
 // ESSA LINHA ABAIXO É A QUE VAI FAZER O SITE ABRIR:
 export const supabaseConfigError = null;
-
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
