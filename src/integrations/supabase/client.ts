cafec71c539fd2e84bbedc44b1fcade42369d29e
raphaelxl <nivelop844@gmail.com>
2026-02-23 15:18:32 -0400

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_URL = "https://bvvlswnwhpbjjhgsoo.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_5iqQHnRSl48pHoK1F466IA_4c12Dg47";
 
-// Esta linha garante que o erro de "not defined" suma do seu navegador
+// Esta linha mata o erro "supabaseConfigError is not defined"
 export const supabaseConfigError = null;
 
 export const supabase = createClient(
